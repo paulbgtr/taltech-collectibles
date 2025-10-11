92c941bb88d29069c0279202a638fc62df16a1b7
paulbgtr <pbg.work@pm.me>
2025-10-11 10:17:12 +0300

    remove gradient for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Pillars } from "@/components/home-page/pillars";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-background to-pink-300 px-4 py-10 flex flex-col items-center">
+    <main className="min-h-screen px-4 py-10 flex flex-col items-center">
       <Introduction />
 
       <CollectiblesShowcase />
