507ba09c8f7d7bb724c0e20f9d49408c197419ca
paulbgtr <pbg.work@pm.me>
2025-10-30 12:26:28 +0200

    configuration for markdown usage
    - install mdx/md deps - disable turbopack (doesn't work with mdx for
    some reason) - add simple md component to check it out

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import createMDX from "@next/mdx";
+
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
