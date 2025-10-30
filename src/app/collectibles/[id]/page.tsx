d05cffed3fcd65ce68a9e301397b2096eebc7c98
paulbgtr <pbg.work@pm.me>
2025-10-30 13:12:49 +0200

    implement model indexing for each keychain

--- src/app/collectibles/[id]/page.tsx
+++ src/app/collectibles/[id]/page.tsx
@@ -2,6 +2,7 @@ import Juulius from "@/markdown/collectibles/juulius.md";
 import Library from "@/markdown/collectibles/library.md";
 import Logo from "@/markdown/collectibles/logo.md";
 import { MDXComponents } from "mdx/types";
+import ModelViewer from "@/components/model-viewer";
 
 const CustomH2 = ({ children }: { children: React.ReactNode }) => (
   <h2 className="text-4xl font-bold mb-4">{children}</h2>
@@ -9,6 +10,13 @@ const CustomH2 = ({ children }: { children: React.ReactNode }) => (
 
 const overrideComponents = {
   h2: CustomH2,
+};
+
+const getModelUrl = (id: string) => {
+  if (id === "juulius") {
+    return `/models/library.glb`;
+  }
+  return `/models/${id}.glb`;
 };
 
 const componentMap: Record<
@@ -30,13 +38,17 @@ export default async function Page({
 
   return (
     <div className="px-4 py-10 flex flex-col items-center min-h-[80vh]">
-      <section className="grid lg:grid-cols-2 max-w-6xl">
+      <section className="grid items-center gap-10 lg:grid-cols-2 max-w-6xl">
         <div>
           {SelectedComponent ? (
             <SelectedComponent components={overrideComponents} />
           ) : (
             <div>Collectible not found.</div>
           )}
+        </div>
+
+        <div className="flex justify-center">
+          <ModelViewer modelUrl={getModelUrl(id)} />
         </div>
       </section>
     </div>
